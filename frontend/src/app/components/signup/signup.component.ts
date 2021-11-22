42b6bf074b1425a372576b76eb5ed07dbd8c8538
Suryakant Vijay <svijay@innominds.com>
2021-11-22 16:14:54 +0530

    validation for signup page

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -6,10 +6,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  username = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+  valid = {
+    username: true,
+    email: true,
+    password: true,
+  };
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  
+  validate(type: string): void {
+    const usernamePattern = /^[\w-.]*$/;
+    const emailPattern = /\S+@\S+\.\S+/;
+
+    if (type === 'username') {
+      if (this.username.length < 5) {
+        this.valid.username = false;
+      } else {
+        this.valid.username = usernamePattern.test(this.username);
+      }
+    } else if (type === 'email') {
+      this.valid.email = emailPattern.test(this.email);
+    } else if (type === ('confirmPassword' || 'password')) {
+      if (this.password !== this.confirmPassword) {
+        this.valid.password = false;
+      } else {
+        this.valid.password = true;
+      }  
+    }
+  
+      
+  
+  }
+  onkey(event: any, type: string) {
+    if (type === 'username') {
+      this.username = event.target.value;
+    } else if (type === 'email') {
+      this.email = event.target.value;
+    } else if (type === 'password') {
+      this.password = event.target.value;
+    } else if (type === 'confirmPassword') {
+      this.confirmPassword = event.target.value;
+    }
+    this.validate(type);
   }
 
 }

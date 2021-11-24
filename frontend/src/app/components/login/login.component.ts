08f92de6027fc7d123f4018b8324790dd0a228db
Suryakant Vijay <svijay@innominds.com>
2021-11-24 13:15:57 +0530

    made changes in main page

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -7,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-
   constructor() { }
 
-  ngOnInit(): void {}
-
+  ngOnInit(): void {
+  }
 
 }
